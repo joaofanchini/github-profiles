c0c950d04a02ee1a38349e71967f1fbd6662b114
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2023-02-16 16:13:45 -0300

    [Impl] Implementação de Dockerfile e Docker Compose para as aplicações

--- github-profiles-back/src/main/java/com/projeto/teste/githubprofilesback/GithubProfilesBackApplication.java
+++ github-profiles-back/src/main/java/com/projeto/teste/githubprofilesback/GithubProfilesBackApplication.java
@@ -3,7 +3,10 @@ package com.projeto.teste.githubprofilesback;
 import com.projeto.teste.githubprofilesback.converters.ConverterFacade;
 import com.projeto.teste.githubprofilesback.converters.impl.Converter;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.boot.web.servlet.server.Encoding;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -17,22 +20,22 @@ import java.util.List;
 @SpringBootApplication
 public class GithubProfilesBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubProfilesBackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubProfilesBackApplication.class, args);
+    }
 
-	@Bean
-	public MessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasenames("messages");
-		messageSource.setDefaultEncoding(StandardCharsets.ISO_8859_1.name());
-		return messageSource;
-	}
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding(StandardCharsets.ISO_8859_1.name());
+        return messageSource;
+    }
 
-	@Bean
-	@Scope("singleton")
-	public ConverterFacade converterFacade(List<Converter> converters){
-		return new ConverterFacade(converters);
-	}
+    @Bean
+    @Scope("singleton")
+    public ConverterFacade converterFacade(List<Converter> converters) {
+        return new ConverterFacade(converters);
+    }
 
 }
